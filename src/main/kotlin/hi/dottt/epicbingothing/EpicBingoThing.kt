7226b3a80b0a567697d83f9266a1e0a82dbf8827
dotPast <83116565+dotPast@users.noreply.github.com>
2024-10-24 17:53:09 +0300

    add init method for interfaces-kotlin

--- src/main/kotlin/hi/dottt/epicbingothing/EpicBingoThing.kt
+++ src/main/kotlin/hi/dottt/epicbingothing/EpicBingoThing.kt
@@ -1,10 +1,12 @@
 package hi.dottt.epicbingothing
 
+import com.noxcrew.interfaces.InterfacesListeners
 import dev.asodesu.origami.engine.Origami
 import org.bukkit.plugin.java.JavaPlugin
 
 class EpicBingoThing : JavaPlugin() {
     override fun onEnable() {
         Origami.init(this)
+        InterfacesListeners.install(this)
     }
 }
