3f374049efb7867443df900994ec562c3d381f38
dotPast <83116565+dotPast@users.noreply.github.com>
2024-10-26 13:49:35 +0300

    add json serializer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.10"
+    kotlin("plugin.serialization") version "2.0.21"
     id("com.gradleup.shadow") version "8.3.2"
     id("xyz.jpenilla.run-paper") version "2.3.1"
 }
@@ -29,6 +30,8 @@ dependencies {
     implementation("com.noxcrew.interfaces:interfaces:1.3.1-SNAPSHOT")
 
     implementation("net.kyori:adventure-text-serializer-plain:4.17.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 }
 
 kotlin {
