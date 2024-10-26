a25e1cbc7be57037e122c1994781da3f1240539d
dotPast <83116565+dotPast@users.noreply.github.com>
2024-10-26 21:35:04 +0300

    implement completion of the item tasks

---
+++ src/main/kotlin/hi/dottt/epicbingothing/behaviors/ItemTaskBehavior.kt
@@ -0,0 +1,61 @@
+package hi.dottt.epicbingothing.behaviors
+
+import dev.asodesu.origami.engine.player.OnlinePlayerBehaviour
+import dev.asodesu.origami.engine.player.PlayerBehaviourContainer
+import dev.asodesu.origami.engine.wiring.annotations.Subscribe
+import hi.dottt.epicbingothing.utility.CardTask
+import io.papermc.paper.event.player.PlayerInventorySlotChangeEvent
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
+import org.bukkit.NamespacedKey
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
+
+class ItemTaskBehavior(container: PlayerBehaviourContainer) : OnlinePlayerBehaviour(container) {
+    fun checkItems(player: Player, item: ItemStack) {
+        val tasksPDC =
+            player.persistentDataContainer.get(NamespacedKey("bingo", "rows"), PersistentDataType.LIST.strings())!!
+
+        val tasks = mutableListOf<String>()
+
+        for (row in tasksPDC) {
+            val rowData = Json.decodeFromString<List<CardTask>>(row)
+
+            for (task in rowData) {
+                if (task.type == CardTask.TYPE.ITEM && !task.completed) {
+                    if (player.inventory.contains(
+                            task.iconMaterial,
+                            task.iconAmount
+                        ) || (item.type == task.iconMaterial && item.amount >= task.iconAmount)
+                    ) {
+                        Bukkit.broadcast(
+                            Component.text("${player.name} marked the ").append(task.getAnnouncementDisplay())
+                                .append(Component.text(" space!"))
+                        )
+                        task.completed = true
+                    }
+                }
+            }
+
+            tasks.add(Json.encodeToString(rowData))
+        }
+
+        player.persistentDataContainer.set(NamespacedKey("bingo", "rows"), PersistentDataType.LIST.strings(), tasks)
+    }
+
+    @Subscribe
+    fun slotChange(event: PlayerInventorySlotChangeEvent) {
+        checkItems(event.player, event.newItemStack)
+    }
+
+    @Subscribe
+    fun inventoryClick(event: InventoryClickEvent) {
+        if (event.currentItem != null) {
+            checkItems(event.viewers[0] as Player, event.currentItem!!)
+        }
+    }
+}
--- src/main/kotlin/hi/dottt/epicbingothing/scenes/MainGameScene.kt
+++ src/main/kotlin/hi/dottt/epicbingothing/scenes/MainGameScene.kt
@@ -4,11 +4,13 @@ import dev.asodesu.origami.engine.add
 import dev.asodesu.origami.engine.player.container
 import dev.asodesu.origami.engine.scene.OnlineGlobalScene
 import hi.dottt.epicbingothing.behaviors.AdvancementTaskBehavior
+import hi.dottt.epicbingothing.behaviors.ItemTaskBehavior
 import org.bukkit.entity.Player
 
 class MainGameScene : OnlineGlobalScene() {
     override fun setupComponents(player: Player) {
         player.container.add<AdvancementTaskBehavior>()
+        player.container.add<ItemTaskBehavior>()
     }
 
     override fun setupComponents() {}
