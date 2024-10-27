b19efb932404aee2d5f545e60fa3b15b32d86826
dotPast <83116565+dotPast@users.noreply.github.com>
2024-10-27 10:52:00 +0300

    make main scene global -> per player

--- src/main/kotlin/hi/dottt/epicbingothing/scenes/MainGameScene.kt
+++ src/main/kotlin/hi/dottt/epicbingothing/scenes/MainGameScene.kt
@@ -2,13 +2,13 @@ package hi.dottt.epicbingothing.scenes
 
 import dev.asodesu.origami.engine.add
 import dev.asodesu.origami.engine.player.container
-import dev.asodesu.origami.engine.scene.OnlineGlobalScene
+import dev.asodesu.origami.engine.scene.OfflinePlayerScene
 import hi.dottt.epicbingothing.behaviors.AdvancementTaskBehavior
 import hi.dottt.epicbingothing.behaviors.ItemTaskBehavior
-import org.bukkit.entity.Player
+import org.bukkit.OfflinePlayer
 
-class MainGameScene : OnlineGlobalScene() {
-	override fun setupComponents(player: Player) {
+class MainGameScene(id: String) : OfflinePlayerScene(id) {
+	override fun setupComponents(player: OfflinePlayer) {
 		player.container.add<AdvancementTaskBehavior>()
 		player.container.add<ItemTaskBehavior>()
 	}
